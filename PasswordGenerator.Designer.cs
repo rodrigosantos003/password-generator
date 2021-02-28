82cccca29084a86382d31489b6251f3c0d5d0add
Rodrigo Santos <79798168+rodrigosantos003@users.noreply.github.com>
2021-02-28 15:38:30 +0000

    Add files via upload

---
+++ PasswordGenerator.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace PasswordGenerator
+{
+    partial class PasswordGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Generate = new System.Windows.Forms.Button();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Generate
+            // 
+            this.btn_Generate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Generate.Location = new System.Drawing.Point(96, 105);
+            this.btn_Generate.Name = "btn_Generate";
+            this.btn_Generate.Size = new System.Drawing.Size(332, 60);
+            this.btn_Generate.TabIndex = 0;
+            this.btn_Generate.Text = "Generate";
+            this.btn_Generate.UseVisualStyleBackColor = true;
+            this.btn_Generate.Click += new System.EventHandler(this.btn_Generate_Click);
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Password.Location = new System.Drawing.Point(12, 30);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.ReadOnly = true;
+            this.txt_Password.Size = new System.Drawing.Size(503, 30);
+            this.txt_Password.TabIndex = 1;
+            this.txt_Password.Text = "Your Generated Password Will Be Displayed Here";
+            this.txt_Password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 200);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.btn_Generate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Password Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Generate;
+        private System.Windows.Forms.TextBox txt_Password;
+    }
+}
+
---
+++ PasswordGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    public partial class PasswordGenerator : Form
+    {
+        private static Random random = new Random();
+
+        public PasswordGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Generate_Click(object sender, EventArgs e)
+        {
+            txt_Password.Clear();
+
+            txt_Password.Text = RandomPass(16);
+        }
+
+        public static string RandomPass(int lenght)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0abcdefghijklmnopqrstuvwxyz123456789_=+-*/.!#%?";
+            return new string(Enumerable.Repeat(chars, lenght).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
